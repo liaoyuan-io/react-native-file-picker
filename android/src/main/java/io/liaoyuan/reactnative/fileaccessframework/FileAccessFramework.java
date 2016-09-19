97d52842e3498ac7e2cd465e71b30e816bd26f22
Zhenjie Ruan <zhenjier@bu.edu>
2016-09-19 17:25:28 -0400

    0.0.1 untested version

---
+++ android/src/main/java/io/liaoyuan/reactnative/fileaccessframework/FileAccessFramework.java
@@ -0,0 +1,25 @@
+package java.io.liaoyuan.reactnative.fileaccessframework;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+class FileAccessFramework extends ReactContextBaseJavaModule {
+
+    FileAccessFramework(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "FileAccessFramework";
+    }
+
+    @ReactMethod
+    public void pickDocument(final Promise promise) {
+        FilePicker picker = new FilePicker(promise);
+        picker.launch();
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import { NativeModules } from 'react-native';
+
+export default module.exports = NativeModules.FileAccessFramework;
